16ee8596b5b797ecd8c87cf814d092ade23fed49
arf20 <aruizfernandez05@gmail.com>
2024-03-16 03:16:13 +0100

    stuff

---
+++ Estadistica/6.R
@@ -0,0 +1,7 @@
+Sistema = 1:2
+Sis = sample(Sistema, 1, prob = c(3/10, 7/10))
+if (Sis == i) { numero = sample(0:9, 6, replace = TRUE)}
+if (Sis == i) { numero = sample(0:9, 7, replace = TRUE)}
+numero
+
+
