85bd23a259e6eda65c1b6ef70064fc06116bb4f1
arf20 <aruizfernandez05@gmail.com>
2024-09-10 08:38:33 +0200

    first commit of 2nd year

--- Estadistica/6.R
+++
@@ -1,8 +0,0 @@
-f=function(x) {
-  if (x < 2.7) return 0;
-  if (x > 3.3) return 0;
-  return k*(x-3)^2;
-}
-
-media = integrate(function(x)x*f(x), 2.7, 3.3);
-varianza = integrate(function(x)x*f(x-media)^2, 2.7, 3.3);
