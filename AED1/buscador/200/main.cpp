f4a96e88418d5cb9ae600c1acbe594a4c417632f
arf20 <aruizfernandez05@gmail.com>
2024-10-15 17:12:57 +0200

    fixed makefile

---
+++ AED1/buscador/200/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <limits>
+
+#include "diccionario.hpp"
+#include "interprete.hpp"
+
+int main() {
+    std::setlocale(LC_ALL, "es_ES.UTF-8"); // trabajar con UTF-8 y caracteres anchos
+
+    Diccionario dic;
+
+    std::wstring in;
+    while (std::wcin >> in) {
+        if (in.length() != 1) continue;
+        int r = interpretar(in[0], dic);
+        if (r < 0) return 0;
+    }
+}
