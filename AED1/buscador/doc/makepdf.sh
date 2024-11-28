a82a537dc7d44b70894396990c5e8ac92d650707
arf20 <aruizfernandez05@gmail.com>
2024-11-28 02:13:45 +0100

    aaaaaaaaaa

--- AED1/buscador/doc/makepdf.sh
+++ AED1/buscador/doc/makepdf.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-iconv -f utf8 -t latin1 memoria.txt | enscript -B -f Courier11 -p - | ps2pdf - - > memoria.enscript.pdf
+iconv -f utf8 -t latin1 memoria.txt | enscript -l -M A4 -f Courier@11 -p - | ps2pdf - - > memoria.pdf
+iconv -f utf8 -t latin1 listing.txt | enscript -l -M A4 -f Courier@6.5/10 -p - | ps2pdf - - > listing.pdf
 
