1bad0423744fbe0e0a11c7258d24b17bc0f52c76
arf20 <aruizfernandez05@gmail.com>
2025-09-23 19:10:07 +0200

    so2 entrega 1

--- SO2/zeos/user.c
+++ SO2/zeos/user.c
@@ -9,13 +9,15 @@ int add(int a, int b)
     return a + b;
 }
 
+extern int addAsm(int a, int b);
+
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    int c = add(69, 420);
+    int c = addAsm(69, 420);
     
-  while(1) { }
+    while(1) { }
 }
