2f2ff7148feb72d31f205868875116bf151b9d96
arf20 <aruizfernandez05@gmail.com>
2025-09-26 14:05:21 +0200

    keyboard done

--- SO2/zeos/entry.s
+++ SO2/zeos/entry.s
@@ -12,3 +12,10 @@
 # 6 "entry.S" 2
 # 1 "include/segment.h" 1
 # 7 "entry.S" 2
+# 73 "entry.S"
+.globl keyboard_handler; .type keyboard_handler, @function; .align 0; keyboard_handler:
+    pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %ebx; pushl %ecx; pushl %edx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es;
+    call keyboard_routine
+    popl %edx; popl %ecx; popl %ebx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs;
+    movb $0x20, %al ; outb %al, $0x20 ;
+    iret
