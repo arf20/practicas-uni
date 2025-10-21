649508d4951837c4add8dbca6768ccc83da45a69
arf20 <aruizfernandez05@gmail.com>
2025-10-22 01:23:40 +0200

    idi lab 2.2, apuntes tema5 so2

---
+++ IDI/lab2.2/MyGLWidget.h
@@ -0,0 +1,38 @@
+// MyGLWidget.h
+#include "BL2GLWidget.h"
+
+#include "model.h"
+
+class MyGLWidget : public BL2GLWidget {
+    Q_OBJECT
+
+    public:
+        MyGLWidget(QWidget *parent=0) : BL2GLWidget(parent) {}
+        ~MyGLWidget();
+
+        void initializeGL();
+        void paintGL();
+        void resizeGL(int width, int height);
+        void keyPressEvent(QKeyEvent *event);
+        void creaBuffers();
+        void modelTransform();
+        void carregaShaders();
+
+        void modelBoundinxBox();
+
+        void init_camera();
+
+        GLuint VAO_model = 0;
+        GLuint VAO_terra = 0;
+
+        GLuint projLoc = 0, viewLoc = 0;
+        Model m;
+
+        float rotacion = 0.0f;
+
+        glm::vec3 model_bounding_box_center;
+
+    private:
+        int printOglError(const char file[], int line, const char func[]);
+};
+
